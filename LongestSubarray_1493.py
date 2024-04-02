8653ca3a5325089404f4caa35a2492590ba537c5
psptsatya24 <147664817+psptsatya24@users.noreply.github.com>
2024-04-02 16:46:06 -0700

    Add files via upload

---
+++ LongestSubarray_1493.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        prevSum, curSum, best, zeroFound = 0, 0, 0, False
+        for i in range(len(nums)):
+            if nums[i] == 1:
+                curSum += 1
+            if nums[i] == 0:
+                best = max(prevSum + curSum, best)
+                prevSum = curSum
+                curSum = 0
+                zeroFound = True
+        return max(curSum + prevSum, best) - 1 if not zeroFound else max(curSum + prevSum, best)
+obj = Solution()
+nums=[0,1,1,1,0,1,1,1,0]
+print(obj.longestSubarray(nums))
